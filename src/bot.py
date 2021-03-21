3c71ef74b2f8a3dee3f6913b23aad9909f82ca2b
Len <leluong@deloitte.com.au>
2021-03-22 01:22:57 +1100

    Bot functioning with 'wotd!' command

--- src/bot.py
+++ src/bot.py
@@ -1,6 +1,8 @@
 import os
 import discord
-
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+from get_wotd import WOTD
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -12,5 +14,26 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content == 'wotd!':
+        response = get_wotd()
+        await message.channel.send(response)
+
+def get_wotd():
+    url = 'https://www.dictionary.com/e/word-of-the-day/'
+    page = urlopen(url)
+
+    html_bytes = page.read()
+    html = html_bytes.decode("utf-8")
+
+    soup = BeautifulSoup(html,features="html.parser")
+    wotd = WOTD(soup)
+    wotd_msg = wotd.print_wotd()
+    return wotd_msg
 
 client.run(TOKEN)
---
+++ src/get_wotd.py
@@ -0,0 +1,27 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+class WOTD:
+    def __init__(self, soup):
+        self.soup = soup
+        self.wotd_string = self.get_wotd()
+        self.wotd_type, self.wotd_definition = self.get_wotd_definition()
+
+    def get_wotd(self):
+        soup = self.soup
+        wotd_headword_divs = soup.find_all("div",{"class":"otd-item-headword__word"})[0]
+        wotd_string = wotd_headword_divs.find('h1').contents[0]
+        return wotd_string
+
+    def get_wotd_definition(self):
+        soup = self.soup
+        def_headword_def_divs = soup.find_all("div",{"class":"otd-item-headword__pos"})[0]
+        def_headword_def_p = def_headword_def_divs.find_all('p')
+        def_type_string = def_headword_def_p[0].contents[1].contents[1].contents[0]
+        def_string = def_headword_def_p[1].contents[0]
+        return def_type_string, def_string
+
+    def print_wotd(self):
+        output = f'> **{self.wotd_string}**\n> *{self.wotd_type}*\n> {self.wotd_definition}'
+        print(output)
+        return output
