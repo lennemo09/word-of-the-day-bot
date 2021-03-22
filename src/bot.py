eae4bc5b61da2f307e8c2bdef577099b2e1059b0
Len <leluong@deloitte.com.au>
2021-03-22 21:50:06 +1100

    Missing commit for auto posting.

--- src/bot.py
+++ src/bot.py
@@ -1,45 +1,105 @@
 import os
 import discord
 import asyncio
+import pytz
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-from get_wotd import WOTD_English
+from get_wotd import WOTD_English, WOTD_Italian, WOTD_German, WOTD_French, WOTD_Russian
 from dotenv import load_dotenv
-from datetime import datetime
+from datetime import datetime, timedelta
+from googletrans import Translator
+
+translator = Translator()
+translator = Translator(service_urls=['translate.googleapis.com'])
+
+POSTING_FREQUENCY = timedelta(hours=24)
+REFRESH_RATE = 5
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+
 client = discord.Client()
+main_channel_id = 755393652791967855
+test_channel_id = 496346061607010304
 wotd_dict = {}
 
+
 async def check_wotd_update_background_task():
+    posting_time = datetime.now(pytz.timezone("Australia/Melbourne")).replace(hour=23, minute=0, second=0, microsecond=0)
+    posted = False
+    yesterday = (datetime.utcnow() - POSTING_FREQUENCY).date()
+
     await client.wait_until_ready()
 
     while not client.is_closed():
         try:
-            await asyncio.sleep(60)
-            print(datetime.utcnow())
-            get_wotd_eng()
-            print(wotd_dict['eng'])
+            await asyncio.sleep(REFRESH_RATE)
+            #print(f'current time: {datetime.now()}')
+            #print(f'posting time: {posting_time}')
+            time = datetime.now(pytz.timezone("Australia/Melbourne"))
 
+            if time.date() != yesterday:
+                print(f"ALERT: New day! {time.date()}")
+                posted = False
+                yesterday = datetime.utcnow().date()
+
+            if time > posting_time and not posted:
+                posted = True
+                posting_time = posting_time + POSTING_FREQUENCY
+                print(f"ALERT: Posted WotD for today at {time}!")
+                print(f'New posting time: {posting_time}')
+
+                response = get_all_wotd()
+                channel = await client.fetch_channel(test_channel_id)
+                await channel.send(response)
 
         except Exception as e:
-            pass
+            print(e)
+
 
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
+
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content == 'wotd!':
-        response = get_wotd_eng()
+    if message.content == 'wotd-english!':
+        response = get_wotd_en()
         await message.channel.send(response)
 
-def get_wotd_eng():
+    if message.content == 'wotd-italian!':
+        response = get_wotd_it()
+        await message.channel.send(response)
+
+    if message.content == 'wotd-german!':
+        response = get_wotd_ge()
+        await message.channel.send(response)
+
+    if message.content == 'wotd!':
+        response = get_all_wotd()
+        await message.channel.send(response)
+
+    if message.content.startswith('wotd!trans '):
+        text = message.content[len('wotd!trans ')::]
+        trans_text, trans_pronunciation = translate(text)
+        await message.channel.send(f"**\"{text}\"** is translated to:\n> {trans_text}\n> Pronunciation: {trans_pronunciation}")
+
+    if message.content == 'wotd!set':
+        main_channel = message.channel.id
+        await message.channel.send("Set as main channel.")
+
+
+def translate(text, src=None, dst='en'):
+    print(f"Received text {text}")
+    translation = translator.translate(text)
+    return (translation.text, translation.pronunciation)
+
+
+def get_wotd_en():
     url = 'https://www.dictionary.com/e/word-of-the-day/'
     page = urlopen(url)
 
@@ -49,8 +109,79 @@ def get_wotd_eng():
     soup = BeautifulSoup(html,features="html.parser")
     wotd = WOTD_English(soup)
     wotd_msg = wotd.wotd_msg()
-    wotd_dict['eng'] = wotd.wotd_string
+    wotd_dict['en'] = wotd_msg
     return wotd_msg
+
+
+def get_wotd_it():
+    url = 'https://www.italianpod101.com/italian-phrases/'
+    page = urlopen(url)
+
+    html_bytes = page.read()
+    html = html_bytes.decode("utf-8")
+
+    soup = BeautifulSoup(html,features="html.parser")
+    wotd = WOTD_Italian(soup)
+    wotd_msg = wotd.wotd_msg()
+    wotd_dict['it'] = wotd_msg
+    return wotd_msg
+
+
+def get_wotd_ge():
+    url = 'https://www.germanpod101.com/german-phrases/'
+    page = urlopen(url)
+
+    html_bytes = page.read()
+    html = html_bytes.decode("utf-8")
+
+    soup = BeautifulSoup(html,features="html.parser")
+    wotd = WOTD_German(soup)
+    wotd_msg = wotd.wotd_msg()
+    wotd_dict['ge'] = wotd_msg
+    return wotd_msg
+
+
+def get_wotd_fr():
+    url = 'https://www.frenchpod101.com/french-phrases/'
+    page = urlopen(url)
+
+    html_bytes = page.read()
+    html = html_bytes.decode("utf-8")
+
+    soup = BeautifulSoup(html,features="html.parser")
+    wotd = WOTD_French(soup)
+    wotd_msg = wotd.wotd_msg()
+    wotd_dict['fr'] = wotd_msg
+    return wotd_msg
+
+
+def get_wotd_ru():
+    url = 'https://www.russianpod101.com/russian-phrases/'
+    page = urlopen(url)
+
+    html_bytes = page.read()
+    html = html_bytes.decode("utf-8")
+
+    soup = BeautifulSoup(html,features="html.parser")
+    wotd = WOTD_Russian(soup)
+    wotd_msg = wotd.wotd_msg()
+    wotd_dict['ru'] = wotd_msg
+    return wotd_msg
+
+
+def get_all_wotd():
+    get_wotd_en()
+    get_wotd_it()
+    get_wotd_ge()
+    get_wotd_fr()
+    get_wotd_ru()
+    print(wotd_dict)
+    output = ""
+    for wotd in wotd_dict.values():
+        output += wotd + '\n\n'
+    print(output)
+    return output
+
 
 if __name__ == "__main__":
     client.loop.create_task(check_wotd_update_background_task())
--- src/get_wotd.py
+++ src/get_wotd.py
@@ -11,17 +11,170 @@ class WOTD_English:
         soup = self.soup
         wotd_headword_divs = soup.find_all("div",{"class":"otd-item-headword__word"})[0]
         wotd_string = wotd_headword_divs.find('h1').contents[0]
+
+        for match in soup.findAll('span'):
+            match.unwrap()
         return wotd_string
 
     def get_wotd_definition(self):
         soup = self.soup
         def_headword_def_divs = soup.find_all("div",{"class":"otd-item-headword__pos"})[0]
+        #print(def_headword_def_divs)
         def_headword_def_p = def_headword_def_divs.find_all('p')
-        def_type_string = def_headword_def_p[0].contents[1].contents[1].contents[0]
-        def_string = def_headword_def_p[1].contents[0]
+        #print(def_headword_def_p)
+
+        #print("Get text")
+        #print(def_headword_def_p.get_text())
+        def_type_string = list(filter(('\n').__ne__, def_headword_def_p[0].contents))[0]
+        def_string = list(filter(('\n').__ne__, def_headword_def_p[1].contents))[0]
+
         return def_type_string, def_string
 
     def wotd_msg(self):
-        output = f'> **{self.wotd_string}**\n> *{self.wotd_type}*\n> {self.wotd_definition}'
+        output = f':flag_gb: ENGLISH:\n> **{self.wotd_string}**\n> *{self.wotd_type}*\n> __Definition:__ {self.wotd_definition}'
         #print(output)
         return output
+
+class WOTD_Italian:
+    def __init__(self, soup):
+        self.soup = soup
+        self.wotd_string = self.get_wotd()
+        self.wotd_type, self.wotd_definition = self.get_wotd_definition()
+
+    def get_wotd(self):
+        soup = self.soup
+        wotd_headword_divs = soup.find_all('div',{'r101-wotd-widget__word'})[0]
+        wotd_string = wotd_headword_divs.contents[0]
+        #print(wotd_string)
+
+        for match in soup.findAll('span'):
+            match.unwrap()
+        return wotd_string
+
+    def get_wotd_definition(self):
+        soup = self.soup
+        def_div = soup.find_all("div",{"class":"r101-wotd-widget__english"})[0]
+        def_string = def_div.contents[0]
+
+        def_type_div = soup.find_all("div",{"class":"r101-wotd-widget__class"})[0]
+        def_type_string = def_type_div.contents[0].strip()
+
+        return def_type_string, def_string
+
+    def wotd_msg(self):
+        output = f':flag_it: ITALIAN:\n> **{self.wotd_string}**\n> *{self.wotd_type}*\n> __Definition:__  {self.wotd_definition}'
+        #print(output)
+        return output
+
+class WOTD_German:
+    def __init__(self, soup):
+        self.soup = soup
+        self.wotd_string = self.get_wotd()
+        self.wotd_type, self.wotd_definition = self.get_wotd_definition()
+
+    def get_wotd(self):
+        soup = self.soup
+        wotd_headword_divs = soup.find_all('div',{'r101-wotd-widget__word'})[0]
+        wotd_string = wotd_headword_divs.contents[0]
+        #print(wotd_string)
+
+        for match in soup.findAll('span'):
+            match.unwrap()
+        return wotd_string
+
+    def get_wotd_definition(self):
+        soup = self.soup
+        def_div = soup.find_all("div",{"class":"r101-wotd-widget__english"})[0]
+        def_string = def_div.contents[0]
+
+        def_type_div = soup.find_all("div",{"class":"r101-wotd-widget__class"})[0]
+        def_type_string = def_type_div.contents[0].strip()
+
+        return def_type_string, def_string
+
+    def wotd_msg(self):
+        output = f':flag_de: GERMAN:\n> **{self.wotd_string.lower()}**\n> *{self.wotd_type}*\n> __Definition:__  {self.wotd_definition}'
+        #print(output)
+        return output
+
+class WOTD_French:
+    def __init__(self, soup):
+        self.soup = soup
+        self.wotd_string = self.get_wotd()
+        self.wotd_type, self.wotd_definition = self.get_wotd_definition()
+
+    def get_wotd(self):
+        soup = self.soup
+        wotd_headword_divs = soup.find_all('div',{'r101-wotd-widget__word'})[0]
+        wotd_string = wotd_headword_divs.contents[0]
+        #print(wotd_string)
+
+        for match in soup.findAll('span'):
+            match.unwrap()
+        return wotd_string
+
+    def get_wotd_definition(self):
+        soup = self.soup
+        def_div = soup.find_all("div",{"class":"r101-wotd-widget__english"})[0]
+        def_string = def_div.contents[0]
+
+        def_type_div = soup.find_all("div",{"class":"r101-wotd-widget__class"})[0]
+        def_type_string = def_type_div.contents[0].strip()
+
+        return def_type_string, def_string
+
+    def wotd_msg(self):
+        output = f':flag_fr: FRENCH:\n> **{self.wotd_string}**\n> *{self.wotd_type}*\n> __Definition:__  {self.wotd_definition}'
+        #print(output)
+        return output
+
+
+class WOTD_Russian:
+    def __init__(self, soup):
+        self.soup = soup
+        self.wotd_string = self.get_wotd()
+        self.wotd_type, self.wotd_definition, self.word_roman = self.get_wotd_definition()
+
+    def get_wotd(self):
+        soup = self.soup
+        wotd_headword_divs = soup.find_all('div',{'r101-wotd-widget__word'})[0]
+        wotd_string = wotd_headword_divs.contents[0]
+        #print(wotd_string)
+
+        for match in soup.findAll('span'):
+            match.unwrap()
+        return wotd_string
+
+    def get_wotd_definition(self):
+        soup = self.soup
+        def_div = soup.find_all("div",{"class":"r101-wotd-widget__english"})[0]
+        def_string = def_div.contents[0]
+
+        def_roman_div = soup.find_all("div",{"class":"r101-wotd-widget__additional-field romanization"})[0]
+        def_roman_string = def_roman_div.contents[0]
+
+        def_type_div = soup.find_all("div",{"class":"r101-wotd-widget__class"})[0]
+        def_type_string = def_type_div.contents[0].strip()
+
+        return def_type_string, def_string, def_roman_string
+
+    def wotd_msg(self):
+        output = f':flag_ru: RUSSIAN:\n> **{self.wotd_string}** | _*Romanization:*_ {self.word_roman}\n> *{self.wotd_type}*\n> __Definition:__  {self.wotd_definition}'
+        #print(output)
+        return output
+
+
+if __name__ == "__main__":
+    #url = 'https://www.dictionary.com/e/word-of-the-day/'
+    url = 'https://www.italianpod101.com/italian-phrases/'
+    page = urlopen(url)
+
+    html_bytes = page.read()
+    html = html_bytes.decode("utf-8")
+
+    soup = BeautifulSoup(html,features="html.parser")
+    #wotd = WOTD_English(soup)
+    wotd = WOTD_Italian(soup)
+    wotd_msg = wotd.wotd_msg()
+
+    print(wotd_msg)
